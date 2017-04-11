4f22235d50e40e95d45cf26e42bc0bf54c6b0c71
Lee Cooper <lee.cooper@drllimited.co.uk>
2017-04-11 21:54:25 +0100

    app running

--- app/App.js
+++ app/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from "react-router"
 import Routes from './Routes'
+import { Provider } from 'react-redux';
 
+import * as budgetActions from './actions/budgetActions';
 
+import configureStore from './store/configureStore';
+const store = configureStore();
+
+store.dispatch(budgetActions.fetchItems());
 
 ReactDOM.render(
-    <Router history={browserHistory} >{Routes}</Router>,
+    <Provider store={store}>
+        <Router routes={Routes} history={browserHistory} />
+    </Provider>,
     document.getElementById('app')
-)
+);
+
 
 
